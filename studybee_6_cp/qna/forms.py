d1c2c642d655b1c833ede82118e330000bdd2250
initialkommit <initialkommit@gmail.com>
2015-08-05 16:04:48 +0900

    registration template dir added.

---
+++ studybee_6_cp/qna/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Email',
+            'required': 'True',
+        }
+    ))
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Email',
+                'required': 'True',
+            }
+        )
+    )
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'required': 'True',
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password confirmation',
+                'required': 'True',
+            }
+        ),
+        help_text="Enter the same password as above, for verification."
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2",)
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
---
+++ studybee_6_cp/studybee_6_cp/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^signup/$', 'qna.views.signup', name='signup'),
+    url(r'^signup_ok/$', TemplateView.as_view(
+        template_name='registration/signup_ok.html'
+    ), name='signup_ok'),
+
+    url(r'^$', 'qna.views.home', name='home'),
+    url(r'^about/$', 'qna.views.about', name='about'),
+    url(r'^q/$', 'qna.views.question', name='question'),
+    url(r'^login/$', 'qna.views.login', name='login'),
+)
