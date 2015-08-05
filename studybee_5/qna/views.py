e66fa07004d60495bc3889336ae5ff51fc2ca247
initialkommit <initialkommit@gmail.com>
2015-08-05 16:04:17 +0900

    registration template dir added.

--- studybee_5/qna/views.py
+++ studybee_5/qna/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+    """signup
+    to register users
+    """
+    if request.method == "POST":
+        userform = UserCreationForm(request.POST)
+        if userform.is_valid():
+            userform.save()
+
+            return HttpResponseRedirect(
+                reverse("signup_ok")
+            )
+    elif request.method == "GET":
+        userform = UserCreationForm()
+
+    return render(request, "registration/signup.html", {
+        "userform": userform,
+    })
+
 def home(request, tag=None):
     return render(request, "index.html")
 
